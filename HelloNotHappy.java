d92213cf64917a6cbe6569244a0e5988dbb72a5f
nazarii_suzansyi <nazarii.suzanskyi@gmail.com>
2023-02-22 13:25:32 +0200

    create document HelloNotHappy

---
+++ HelloNotHappy.java
@@ -0,0 +1,22 @@
+package happy.not.hello;
+
+public class HelloNotHappy {
+
+	public static void main(String[] args) {
+		System.out.print ("H");
+		System.out.print("e");
+		System.out.print("l");
+		System.out.print("l");
+		System.out.println("o");
+		System.out.print ("    ");
+		System.out.println("N" + "o" + "t");
+		System.out.print("      "); 
+		System.out.println("H" + "a" + "p" + "p" + "y");
+		System.out.print("          ");
+		System.out.println("This");
+		System.out.print ("             ");
+		System.out.println("is");
+		System.out.println ("               " + "BAD");
+	}
+
+}
